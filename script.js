dc70c3efd2c4b3db5d6093c54bf71ea7765ae045
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2023-04-28 13:05:33 +0800

    Complete functionality
    All functionality has been tested and works as expected.
    Next steps will be to style the game for a better user experience.

--- script.js
+++ script.js
@@ -1,255 +1,218 @@
-function disableContainer(containerToDisable) {
-  containerToDisable.style.display = 'none';
-};
+//PRE-CONTENT
+const containerPreStart = document.createElement('div');
+const buttonStartGame = document.createElement('button');
+// MAIN CONTENT
+const containerMain = document.createElement('div');
+const containerButtonsChoice = document.getElementById('buttons-container');;
+const buttonsChoice = document.querySelectorAll('.buttonsChoice');
+// MODAL WINDOW
+const containerModal = document.createElement('div');
+const contentModal = document.createElement('div');
+const buttonNewGame = document.createElement('button');
+const buttonClose = document.createElement('button');
+// CONTENT AFTER 'CLOSE' BUTTON
+const containerAfterClose = document.createElement('div');
+const buttonHomePage = document.createElement('button');
 
-function enableContainer(containerToEnable) {
-  containerToEnable.style.display = 'flex';
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1; 
+
+containerPreStart.setAttribute('id', 'container-prestart');
+containerPreStart.setAttribute('class', 'containers-prestart containers');
+buttonStartGame.setAttribute('id', 'buttonStartGame');
+buttonStartGame.setAttribute('class', 'buttons');
+buttonStartGame.innerText = 'START GAME';
+containerMain.setAttribute('id', 'container-main');
+containerMain.setAttribute('class', 'containers-main containers');
+containerMain.style.display = 'none';
+containerMain.appendChild(containerModal);
+containerModal.setAttribute('id', 'container-modal');
+containerModal.setAttribute('class', 'containers-modal containers');
+contentModal.setAttribute('id', 'content-modal');
+contentModal.setAttribute('class', 'containers-modal containers');
+buttonNewGame.setAttribute('id', 'button-newgame');
+buttonNewGame.setAttribute('class', 'buttons-modal buttons');
+buttonNewGame.innerText = 'NEW GAME';
+buttonNewGame.disabled = true;
+buttonClose.setAttribute('id', 'button-close');
+buttonClose.setAttribute('class', 'buttons-modal buttons');
+buttonClose.innerText = 'CLOSE';
+buttonClose.disabled = true;
+containerAfterClose.setAttribute('id', 'container-afterclose');
+containerAfterClose.setAttribute('class', 'containers-afterclose containers');
+containerAfterClose.style.display = 'none';
+buttonHomePage.setAttribute('id', 'button-homepage');
+buttonHomePage.setAttribute('class', 'buttons-afterclose buttons');
+buttonHomePage.innerText = 'BACK TO HOME PAGE';
+buttonHomePage.disabled = true;
+
+
+
+document.body.appendChild(containerPreStart);
+containerPreStart.appendChild(buttonStartGame);
+document.body.appendChild(containerMain);
+containerMain.appendChild(containerButtonsChoice);
+containerMain.appendChild(containerModal);
+containerModal.appendChild(contentModal);
+contentModal.appendChild(buttonNewGame);
+contentModal.appendChild(buttonClose);
+document.body.appendChild(containerAfterClose);
+containerAfterClose.appendChild(buttonHomePage);
+
+buttonStartGame.addEventListener('click', () => {
+  containerPreStart.style.display = 'none';
+  containerMain.style.display = 'flex';
+});
+
+buttonsChoice.forEach(button => {
+  button.addEventListener('click', handleButtonsChoiceClick);
+});
+
+buttonNewGame.addEventListener('click', newGame);
+buttonClose.addEventListener('click', moveToCloseContent);
+buttonHomePage.addEventListener('click', moveToMainPage);
+
+function disableButtonsChoice() {
+  buttonsChoice.forEach(button => {
+    button.disabled = true;;
+  })
+}
+
+function enableButtonsChoice() {
+  buttonsChoice.forEach(button => {
+    button.disabled = false;;
+  })
 };
 
 function showModalWindow() {
   containerModal.style.display = 'flex';
   contentModal.style.display = 'flex';
+  buttonNewGame.disabled = false;
+  buttonClose.disabled = false;
 };
 
 function hideModalWindow() {
   containerModal.style.display = 'none';
   contentModal.style.display = 'none';
+  buttonNewGame.disabled = true;
+  buttonClose.disabled = true;
 }
 
-function getComputerSelection() {
-    const options = ['rock', 'paper', 'scissors'];
-    let choice = options[Math.floor(Math.random() * options.length)];
-    choiceComputer.innerText = `Computer's choice: ${capitaliseFirstLetter(choice)}`;
+function moveToCloseContent() {
+  hideModalWindow();
+  containerMain.style.display = 'none';
+  buttonNewGame.disabled = true;
+  containerAfterClose.style.display = 'flex';
+  buttonHomePage.disabled = false;
+};
 
-    return choice;
+function moveToMainPage() {
+  window.location.reload();
+}
+
+function handleButtonsChoiceClick(event) {
+  const playerChoice = event.target.innerText.toLowerCase();
+  playGame(playerChoice);
+};
+
+function getComputerChoice() {
+  const options = ['rock', 'paper', 'scissors'];
+  return options[Math.floor(Math.random() * options.length)];
 };
 
 function capitaliseFirstLetter(word) {
-    return newWord = word[0].toUpperCase() + word.slice(1,);
+  return word[0].toUpperCase() + word.slice(1,);
 };
 
-function getRoundResult(roundResult, playerSign, computerSign) {
-    let detailedResult = roundResult;
-
-    if (roundResult === "You win! ") {
-        detailedResult += `${capitaliseFirstLetter(playerSign)} ${playerSign === "scissors" ? "beat" : "beats"} ${
-            capitaliseFirstLetter(computerSign)}`;
-    }
-    else {
-        detailedResult += `${capitaliseFirstLetter(computerSign)} ${computerSign === "scissors" ? "beat" : "beats"} ${
-            capitaliseFirstLetter(playerSign)}`;
-    }
-
-    return detailedResult;
+function getRoundComment(resultToComment, playerSelection, computerSelection) {
+  if (resultToComment === 'You win!') {
+    return `${capitaliseFirstLetter(playerSelection)} ${playerSelection === 'scissors' ? 'beat' : 'beats'} ${
+        capitaliseFirstLetter(computerSelection)}`;
+      }
+  else {
+    return `${capitaliseFirstLetter(computerSelection)} ${computerSelection === 'scissors' ? 'beat' : 'beats'} ${
+        capitaliseFirstLetter(playerSelection)}`;
+      }
 };
 
-function playRound(playerSelection) {
-    const computerSelection = getComputerSelection();
-    let answer = '';
-    let finalAnswer = '';
-
-    if (playerSelection === computerSelection) {
-        finalAnswer = `Draw! You both have selected ${capitaliseFirstLetter(playerSelection)}`;
-    }
-    else {
-        switch (playerSelection) {
-            case "rock":
-                if (computerSelection === "paper") {
-                    answer = "You lose! ";
-                }
-                else {
-                    answer = "You win! ";
-                }
-                break;
-            case "paper":
-                if (computerSelection === "rock") {
-                    answer = "You win! ";
-                }
-                else {
-                    answer = "You lose! ";
-                }
-                break;
-            case "scissors":
-                if (computerSelection === "rock") {
-                    answer = "You lose! ";
-                }
-                else {
-                    answer = "You win! ";
-                }
-                break;
-        }
-
-        finalAnswer= getRoundResult(answer, playerSelection, computerSelection);
-    }
-    resultRound.innerText = finalAnswer;
-    return finalAnswer;
+function updateScore(resultNew) {
+  if (resultNew === 'You win!') {
+    playerScore++;
+  }
+  else if (resultNew === 'Computer wins!') {
+    computerScore++;
+  }
 };
 
-let playerScore = 0;
-let computerScore = 0;
-let currentRound = 1;
-
-function resetGame() {
-  // Reset the scores and current round
+function newGame(){
   playerScore = 0;
   computerScore = 0;
-  currentRound = 1;
-
-  // Clear the result and final elements
-  resultRound.innerText = '';
-  finalResult.innerText = '';
-  finalVerdict.innerText = '';
-
-  // Enable the game buttons
-  buttonsChoice.forEach(button => {
-    button.disabled = false;
-  });
-
-  // Set the initial score text
-  scoreCurrent.innerText = `Score: ${playerScore} : ${computerScore}`;
-  numberRound.innerText = `ROUND ${currentRound}`;
-  playGame(playerScore, computerScore, currentRound); ///
-}
-
-function playGame(playerScore, computerScore, currentRound) {
-  // Call the resetGame function to reset the game
-  //resetGame();
-  console.log(`Score before the game: ${playerScore}, ${computerScore}`);
+  roundNumber = 1;
   
-
-  buttonsChoice.forEach(button => {
-    button.addEventListener('click', (event) => {
-      const playerChoice = event.target.textContent.toLowerCase();
-      const resultCurrent = playRound(playerChoice);
-      console.log(resultCurrent);
-      
-      numberRound.innerText = `ROUND ${currentRound}`;
-      currentRound++;
-      choicePlayer.innerText = `Your choice: ${capitaliseFirstLetter(playerChoice)}`;
-
-      if (resultCurrent.startsWith('You win!')) {
-        playerScore++;
-      } 
-      else if (resultCurrent.startsWith('You lose!')) {
-        computerScore++;
-      } 
-      else {
-        playerScore++;
-        computerScore++;
-      };
-      scoreCurrent.innerText = `Score: ${playerScore} : ${computerScore}`;
-      console.log(`player score: ${playerScore}, computer score: ${computerScore}`);
-
-      if (playerScore === 5 || computerScore === 5) {
-        finalResult.innerText = `FINAL SCORE: ${playerScore} : ${computerScore}`;
-        console.log(finalResult);
-        if (playerScore > computerScore) {
-          finalVerdict.innerText = 'Congratulations, you win!';
-        } else if (computerScore > playerScore) {
-          finalVerdict.innerText = 'Sorry, you lose!';
-        } else {
-          finalVerdict.innerText = 'It\'s a tie!';
-        }
-
-        // Disable the buttons to prevent further play
-        buttonsChoice.forEach(button => {
-          button.disabled = true;
-        });
-
-        showModalWindow();   
-        return;    
-      }
-    });
-  });
+  enableButtonsChoice();
+  hideModalWindow();
 };
 
-// PRESTART CONTENT
-const containerPreStart = document.createElement('div');
-const greetings = document.createElement('div');
-const buttonStartGame = document.createElement('button');
-greetings.innerText = 'WELCOME TO ROCK-SCISSORS-PAPER!'
-buttonStartGame.innerText = 'START';
-document.body.appendChild(containerPreStart);
-containerPreStart.appendChild(greetings);
-containerPreStart.appendChild(buttonStartGame);
-        
-// MAIN CONTENT
-const containerMain = document.createElement('div');
-document.body.appendChild(containerMain);
+function playGame(playerSign) {
+  let computerSign = getComputerChoice();
+  let roundResult = 'It\'s a tie!';
+  let roundComment = `You both have selected ${capitaliseFirstLetter(playerSign)}`;
 
-const buttonsChoice = document.querySelectorAll('.buttonChoice');
-buttonsChoice.forEach(button => {
-  containerMain.appendChild(button)
-});
-disableContainer(containerMain);
-// Round information
-const containerRound = document.createElement('div');
-const containerChoices = document.createElement('div');
-const numberRound = document.createElement('p');
-const choicePlayer = document.createElement('p');
-const choiceComputer = document.createElement('p');
-const resultRound= document.createElement('p');
-const scoreCurrent = document.createElement('p');
-containerMain.appendChild(containerRound);
-containerRound.appendChild(containerChoices);
-containerRound.appendChild(numberRound);
-containerChoices.appendChild(choicePlayer);
-containerChoices.appendChild(choiceComputer);
-containerRound.appendChild(resultRound);
-containerRound.appendChild(scoreCurrent);
-// Final information
-const containerModal = document.createElement('div');
-const contentModal = document.createElement('div');
+  console.log(`Round: ${roundNumber}`);
+  console.log(`Your choice: ${capitaliseFirstLetter(playerSign)}`);
+  console.log(`Computer choice: ${capitaliseFirstLetter(computerSign)}`);
 
-const finalResult = document.createElement('p');
-const finalVerdict = document.createElement('p');
-const messageFinal = document.createElement('p');
-const buttonNewGame = document.createElement('button');
-const buttonClose = document.createElement('button');
-buttonClose.textContent = 'Close';
-containerModal.setAttribute('id', 'modal-overlay');
-contentModal.setAttribute('id', 'modal');
-messageFinal.textContent = 'The game has ended. What would you like to do?';
-buttonNewGame.textContent = 'New Game';
-containerMain.appendChild(containerModal);
-containerModal.appendChild(contentModal);
-contentModal.appendChild(finalResult);
-contentModal.appendChild(finalVerdict);
-contentModal.appendChild(messageFinal);
-contentModal.appendChild(buttonNewGame);
-contentModal.appendChild(buttonClose);
+  if (playerSign !== computerSign) {
+    switch (playerSign) {
+      case 'rock':
+        if (computerSign === 'paper') {
+          roundResult = 'Computer wins!';
+        }
+        else {
+          roundResult = 'You win!';
+        }
+        break;
+      case 'paper':
+        if (computerSign === 'rock') {
+          roundResult = 'You win!';
+        }
+        else {
+          roundResult = 'Computer wins!';
+        }
+        break;
+      case 'scissors':
+        if (computerSign === 'rock') {
+          roundResult = 'Computer wins!';
+        }
+        else {
+          roundResult = 'You win!';
+        }
+        break;
+    }
+    roundComment = getRoundComment(roundResult, playerSign, computerSign);
+  }
+  
+  updateScore(roundResult);
+  roundNumber++;
 
-// CONTENT AFTER CLICKING THE 'CLOSE BUTTON'
-const containerAfterClose = document.createElement('div');
-const messageAfterClose = document.createElement('div');
-const buttonMainContent = document.createElement('button'); 
-buttonMainContent.innerText = 'COME BACK TO THE MAIN PAGE';
-messageAfterClose.innerText = 'Thank you for the Game!'
-document.body.appendChild(containerAfterClose);
-containerAfterClose.appendChild(messageAfterClose);
-containerAfterClose.appendChild(buttonMainContent);
-containerAfterClose.style.display = 'none';
-disableContainer(containerAfterClose);
+  console.log('Round result:',roundResult, roundComment);
+  console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`);
 
+  if (playerScore === 5 || computerScore === 5) {
+    console.log('Game Over!');
+    console.log(`Final Score:\nPlayer:${playerScore}\nComputer${computerScore}`);
 
-
-buttonNewGame.addEventListener('click', () => {
-    hideModalWindow() 
-    resetGame();
-  });
-
-buttonClose.addEventListener('click', () => {
-    containerMain.style.display = 'none';
-    containerAfterClose.style.display = 'flex';
-});
-
-buttonStartGame.addEventListener('click', () => {
-  disableContainer(containerPreStart);
-  enableContainer(containerMain);
-});
-
-buttonMainContent.addEventListener('click', () => {
-  window.location.reload();
-});
-
-resetGame();
+    if (playerScore > computerScore) {
+      console.log('You win the game!');
+    } 
+    else if (playerScore < computerScore) {
+      console.log('The computer wins the game!');
+    } 
+    else {
+      console.log('The game ends in a tie!');
+    }
+    disableButtonsChoice();
+    showModalWindow();
+  }
+};
